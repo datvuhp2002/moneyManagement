8e3cf70d558e6aca0bc1387eb3be5d0a6e1179ac
Đạt Vũ <datvuhp2002@gmail.com>
2024-02-28 01:30:23 +0700

    fix statergies for auth api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,26 +1,44 @@
-import { Body, Controller, Post, SetMetadata, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+  Get,
+  Req,
+} from '@nestjs/common';
 import { LoginUserDto, RegisterUserDto } from './dto/registerUserDto.dto';
 import { AuthService } from './auth.service';
 import { User } from '@prisma/client';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from './decorator/auth.decorator';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Request } from 'express';
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
-    @Post('register')
-    @SetMetadata('isPublic', true)
-    register(@Body() body:RegisterUserDto ):Promise<User>{
-        return this.authService.register(body)
-    }
-    @Post('login')
-    @SetMetadata('isPublic', true)
-    @UsePipes(ValidationPipe)
-    login(@Body() body:LoginUserDto ):Promise<User>{
-        return this.authService.login(body)
-    }
-    @Post('refresh_token')
-    @SetMetadata('isPublic', true)
-    refreshToken(@Body() {refresh_token}):Promise<any>{
-        return this.authService.refreshToken(refresh_token)
-    }
+  constructor(private authService: AuthService) {}
+  @Post('register')
+  @Public()
+  register(@Body() body: RegisterUserDto): Promise<User> {
+    return this.authService.register(body);
+  }
+  @UseGuards(AuthGuard('local'))
+  @Public()
+  @Post('login')
+  async login(@Req() req: Request) {
+    return await this.authService.login(req.user);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request) {
+    return req.user;
+  }
+  @Post('refresh_token')
+  @Public()
+  refreshToken(@Body() { refresh_token }): Promise<any> {
+    return this.authService.refreshToken(refresh_token);
+  }
 }
