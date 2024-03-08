6a93782182b1f688d0bc5acae1cf3187824c11b3
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-03-08 09:26:55 +0700

    api for transaction

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,7 +1,20 @@
-import { IsNotEmpty } from "class-validator";
-
+import { IsNotEmpty, IsNumber, IsOptional } from "class-validator";
+  
 export class CreateTransactionDto {
     @IsNotEmpty()
     transactionType: string
-    
+    @IsNotEmpty()
+    bill:number
+    @IsNotEmpty()
+    note: string
+    @IsNotEmpty()
+    currency_id:number
+    @IsNotEmpty()
+    categoriesGroup_id:number
+    @IsNotEmpty()
+    category_id:number
+    @IsNotEmpty()
+    wallet_id:number
+    @IsOptional()
+    paymentImage: string
 }
