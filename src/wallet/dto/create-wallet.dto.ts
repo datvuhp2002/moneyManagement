e0ef2469fb40ba2fc91f5dde6492a12fd18d00be
DuonggPh <104338962+DuonggPh@users.noreply.github.com>
2024-03-11 00:14:49 +0700

    wallet_controller

--- src/wallet/dto/create-wallet.dto.ts
+++ src/wallet/dto/create-wallet.dto.ts
@@ -1,1 +1,22 @@
-export class CreateWalletDto {}
+import { IsNotEmpty } from "class-validator";
+export class CreateWalletDto {
+    @IsNotEmpty()
+    name:string;
+    @IsNotEmpty()
+    amount:number;
+}
+export interface WalletFilterType {
+    items_per_page?: number;
+    page?: number;
+    search?: string;
+    nextPage?: number;
+    previousPage?: number;
+  }
+export interface WalletPaginationResponseType {
+    data: { name: string; createdAt: Date }[];
+    total: number;
+    currentPage: number;
+    nextPage?: number;
+    previousPage?: number;
+    itemsPerPage?: number;
+}
--- src/wallet/dto/update-wallet.dto.ts
+++ src/wallet/dto/update-wallet.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateWalletDto } from './create-wallet.dto';
 
-export class UpdateWalletDto extends PartialType(CreateWalletDto) {}
+export class UpdateWalletDto extends PartialType(CreateWalletDto) {
+    name:string;
+    amount:number;
+}
