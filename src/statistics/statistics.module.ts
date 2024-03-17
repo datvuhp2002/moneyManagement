7e4d0183faa327f74a3e199107b1536e578e1ad9
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-03-17 17:35:28 +0700

    Khi xoá một ví thì các giao dịch và thống kê liên quan đến ví cũng được xoá theo

--- src/statistics/statistics.module.ts
+++ src/statistics/statistics.module.ts
@@ -4,7 +4,6 @@ import { StatisticsController } from './statistics.controller';
 import { PrismaService } from 'src/prisma.servcie';
 import { TransactionService } from 'src/transaction/transaction.service';
 import { WalletService } from 'src/wallet/wallet.service';
-
 @Module({
   controllers: [StatisticsController],
   providers: [StatisticsService, PrismaService,TransactionService,WalletService],
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
 import { PrismaService } from 'src/prisma.servcie';
+import { StatisticsService } from 'src/statistics/statistics.service';
 
 @Module({
   controllers: [WalletController],
